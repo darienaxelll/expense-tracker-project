3ecb32ed60e804d3357a7592b1a7b51db04e5201
Darien Salazar <darien.axell.salazar@gmail.com>
2023-08-14 21:16:04 -0500

    Completed expense tracker app

--- src/components/Chart/ChartBar.js
+++ src/components/Chart/ChartBar.js
@@ -1,7 +1,7 @@
 import "./ChartBar.css";
 
 const ChartBar = (props) => {
-  const barFillHeight = "0%";
+  let barFillHeight = "0%";
 
   if (props.maxValue > 0) {
     barFillHeight = Math.round((props.value / props.maxValue) * 100) + "%";
